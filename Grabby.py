3ce46dca3e39fd27fbf9f75497a767ecef221449
loganfarrow <f.logan@wustl.edu>
2023-05-08 17:26:27 -0500

    Completely rewrote how screenshots are handled adding a new class for multiple monitor support, also added a new design for the Tkinter GUI

--- Grabby.py
+++ Grabby.py
@@ -1,117 +1,169 @@
-import tkinter as tk
-from tkinter import ttk
-from PIL import Image, ImageGrab, ImageTk, ImageOps, ImageFilter
-import pyscreenshot as ImageGrab
-import numpy as np
-import time
-import sv_ttk
+import customtkinter
+import os
+from PIL import Image
 
-class App(tk.Tk):
+import pytesseract
+import cv2
+
+
+
+
+
+import ctypes
+
+from screeninfo import get_monitors
+
+#1
+from Screenshot import Screenshot 
+
+
+
+#2
+class App(customtkinter.CTk, Screenshot):
     def __init__(self):
         super().__init__()
-        self.title("Text Sniper")
-        self.geometry("300x200")
+
+        self.title("Grabby")
+        self.geometry("650x350")
+        self.minsize(650, 350)
+
+        # Initialize windows list and x1, y1 variables
+        self.windows = []
+        self.x1 = None
+        self.y1 = None
 
 
-        # Apply the sv-ttk theme
-        sv_ttk.set_theme("dark")
+        #get scaling information for more compatability 
+       # ctypes.windll.shcore.SetProcessDpiAwareness(2)
+        user32 = ctypes.windll.user32
+        screen_dpi = user32.GetDpiForSystem()
+
+
+        # Calculate the scaling factor
+        self.scaling_factor = screen_dpi / 96.0  # 96 DPI is the standard for most displays
+
+        # set grid layout 1x2
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        # load images with light and dark mode image
+        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_images")
+        self.logo_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "CustomTkinter_logo_single.png")), size=(26, 26))
+        self.large_test_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "large_test_image.png")), size=(500, 150))
+        self.image_icon_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "image_icon_light.png")), size=(20, 20))
+        self.home_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "home_dark.png")),
+                                                 dark_image=Image.open(os.path.join(image_path, "home_light.png")), size=(20, 20))
+        self.chat_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "paperblack.png")),
+                                                 dark_image=Image.open(os.path.join(image_path, "paperwhite.png")), size=(20, 20))
+        self.add_user_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "cog.png")),
+                                                     dark_image=Image.open(os.path.join(image_path, "coglight.png")), size=(20, 20))
+
+        # create navigation frame
+        self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
+        self.navigation_frame.grid(row=0, column=0, sticky="nsew")
+        self.navigation_frame.grid_rowconfigure(4, weight=1)
+
+        self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="  Grabby", image=self.logo_image,
+                                                             compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
+        self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
+
+
+
+        #home button
+        self.home_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Home",
+                                                   fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                   image=self.home_image, anchor="w", command=self.home_button_event)
+        self.home_button.grid(row=1, column=0, sticky="ew")
+
+
         
 
-        # Create a 'Grab' button and add it to the main window
-        self.grab_button = ttk.Button(self, text="Grab", command=self.grab_screen)
-        self.grab_button.place(relx=0.5, rely=0.5, anchor='center')
-
-
-
-    def grab_screen(self):
-        # Hide the main window and update the UI
-        self.withdraw()
-        self.update()
-        time.sleep(.01)
-
-        # Take a screenshot and apply a blur filter to create a darkened background
-        self.full_screen_img = ImageGrab.grab()  # Store the original screen image
-        screen_width = self.winfo_screenwidth()
-        screen_height = self.winfo_screenheight()
-        self.full_screen_img = self.full_screen_img.resize((screen_width, screen_height))
-
-        blurred_screen_img = self.full_screen_img.filter(ImageFilter.GaussianBlur(0.1))
-
-        self.create_overlay(self.full_screen_img)
 
 
 
 
+        self.history_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="History",
+                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                      image=self.chat_image, anchor="w", command=self.history_button_event)
+        self.history_button.grid(row=2, column=0, sticky="ew")
 
-    def create_overlay(self, screen_img):
-        overlay = tk.Toplevel(self)
-        overlay.attributes("-fullscreen", True)
-        overlay.attributes("-alpha", 1)
-        overlay.configure(bg="black")
+        self.frame_3_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Settings",
+                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                      image=self.add_user_image, anchor="w", command=self.frame_3_button_event)
+        self.frame_3_button.grid(row=3, column=0, sticky="ew")
 
-        canvas = tk.Canvas(overlay, highlightthickness=0)
-        canvas.pack(fill="both", expand=True)
+        self.appearance_mode_menu = customtkinter.CTkOptionMenu(self.navigation_frame, values=["Light", "Dark", "System"],
+                                                                command=self.change_appearance_mode_event)
+        self.appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
 
-        self.tk_img = ImageTk.PhotoImage(screen_img)
-        canvas.create_image(0, 0, anchor="nw", image=self.tk_img)
+        # create home frame
+        self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.home_frame.grid_columnconfigure(0, weight=1)
 
-        canvas.bind("<Button-1>", self.on_button_press)
-        canvas.bind("<B1-Motion>", self.on_move_press)
-        canvas.bind("<ButtonRelease-1>", self.on_button_release)
+        #self.home_frame_large_image_label = customtkinter.CTkLabel(self.home_frame, text="", image=self.large_test_image)
+        #self.home_frame_large_image_label.grid(row=0, column=0, padx=20, pady=10)
 
-        self.start_x = None
-        self.start_y = None
-        self.rect_id = None
-
-        overlay.mainloop()
-
-
-    def on_button_press(self, event):
-        # Save the initial mouse press coordinates and draw a rectangle on the canvas
-        self.start_x = event.x
-        self.start_y = event.y
-        self.rect_id = event.widget.create_rectangle(self.start_x, self.start_y, event.x, event.y, outline="red")
-
-    def on_move_press(self, event):
-        # Update the rectangle coordinates while the user is dragging the mouse
-        curX = event.x
-        curY = event.y
-        event.widget.coords(self.rect_id, self.start_x, self.start_y, curX, curY)
-
-    def on_button_release(self, event):
+        self.home_frame_button_1 = customtkinter.CTkButton(self.home_frame, text="Capture Text", text_color=("gray10", "gray90"),width=100, height=50,command=self.capture_text_button)
+        self.home_frame_button_1.grid(row=0, column=0, padx=20, pady=145)
         
 
-        # Determine the screenshot coordinates based on the drawn rectangle
-        x1 = min(self.start_x, event.x)
-        y1 = min(self.start_y, event.y)
-        x2 = max(self.start_x, event.x)
-        y2 = max(self.start_y, event.y)
+        # create second frame
+        self.second_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
 
-        # Capture the screenshot using the coordinates
-        screenshot = self.full_screen_img.crop((x1, y1, x2, y2))
-        # Add your OpenCV logic here
+        # create third frame
+        self.third_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
 
-        # Close the overlay window
-        event.widget.master.destroy()
+        # select default frame
+        self.select_frame_by_name("home")
+    
+
+    def capture_text_button(self):
+        #get the screenshots for each monitor as a list
+        images = self.grab_screenshots()
+
+        for idx, screen in enumerate(get_monitors()):
+            window = self.create_image_window(screen, images[idx], idx)
+            self.windows.append(window)
+
+    #def extract_text(self,img):
         
-        # Show the main window again and update the UI
-        self.update()
-        self.deiconify()
 
 
-        # Display the captured screenshot in a new window
-        self.show_screenshot(screenshot)
 
-    def show_screenshot(self, screenshot):
-        # Create a new window to display the captured screenshot
-        screenshot_window = tk.Toplevel(self)
-        screenshot_window.title("Captured Screenshot")
+    def select_frame_by_name(self, name):
+        # set button color for selected button
+        self.home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
+        self.history_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
+        self.frame_3_button.configure(fg_color=("gray75", "gray25") if name == "frame_3" else "transparent")
 
-        # Convert the screenshot to a PhotoImage and display it in a label
-        img = ImageTk.PhotoImage(screenshot)
-        label = tk.Label(screenshot_window, image=img)
-        label.image = img
-        label.pack()
+        # show selected frame
+        if name == "home":
+            self.home_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.home_frame.grid_forget()
+        if name == "frame_2":
+            self.second_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.second_frame.grid_forget()
+        if name == "frame_3":
+            self.third_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.third_frame.grid_forget()
+
+    def home_button_event(self):
+        self.select_frame_by_name("home")
+
+    def history_button_event(self):
+        self.select_frame_by_name("frame_2")
+
+    def frame_3_button_event(self):
+        self.select_frame_by_name("frame_3")
+
+    def change_appearance_mode_event(self, new_appearance_mode):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+    
+
+
 
 if __name__ == "__main__":
     app = App()
